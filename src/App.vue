4d06747ed4869fe226f1f31824bed5e7d8bf0fa5
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-12-13 11:44:45 +0100

    struttura, font

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
+    <Header />
+    <Main />
+    <Footer />
 
   </div>
 </template>
 
 <script>
 
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+
 
 export default {
   name: 'App',
   components: {
+    Header,
+    Main,
+    Footer
 
   }
 }
@@ -17,4 +28,17 @@ export default {
 
 <style lang="scss">
 
+@import '~bootstrap/scss/bootstrap.scss';
+@import '~@fontsource/merriweather/index.css';
+@import '~@fontsource/merriweather/700.css';
+@import './assets/style/vars.scss';
+
+
+// body{
+//   font-family: $title-font;
+// }
+
+
 </style>
+
+
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    HEADER
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+//IMPORT STYLE
+@import '../assets/style/vars.scss';
+
+</style>
